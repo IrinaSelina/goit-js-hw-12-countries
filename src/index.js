523682bf7fcb6e9a1481b303edab4ba427c6cec5
Irina Selina <iras198308@gmail.com>
2021-04-19 00:20:35 +0300

    Делает поиск стран

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './css/styles.css';
 import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css'
 import api from './api-service';
 import countryCardTpl from './templates/country-card.hbs';
 import countryListTpl from './templates/many-countries-card.hbs';
 const { error } = require('@pnotify/core');
 var debounce = require('lodash.debounce');
+
 
 const form = document.querySelector('.form')
 const list = document.querySelector('.list')
@@ -15,22 +17,33 @@ function searchCountry(e) {
     e.preventDefault()
     clearArticlesContainer()
     const searchQuery = e.target.value;
-  console.log(searchQuery);
+
 
   api.fetchCountries(searchQuery).then(data => {
     if (data.length > 10) {
       error({
-        text: "Too many matches found. Please enter a more specific query!"
-});
-        }
-        if (data.length === 1) {
+        text: "Too many matches found. Please enter a more specific query!",
+         delay: 3000
+      })
+    }
+    else if (data.status === 404) {
+      error({
+        text: "No country has been found. Please enter a more specific query!",
+        delay: 3000
+      })
+    }
+    else if (data.length === 1) {
             createsMarcup(data, countryCardTpl )
         }
-        if (data.length > 1 && data.length <= 10) {
+        else if(data.length > 1 && data.length <= 10) {
         createsMarcup(data, countryListTpl )
         }
-    }).catch()
-    
+  }).catch(Error => {
+    Error({
+      text: "You must enter query parameters!",
+      delay: 3000
+    })
+  })  
 }
 function createsMarcup (countries, templates) {
     const marcup = countries.map(country => templates(country) 
